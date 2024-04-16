87ddd3c47801ebd357dea5efd4705d74fba91953
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2024-04-17 01:30:31 +0900

    Set platforms to newer ones

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Tardiness",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
