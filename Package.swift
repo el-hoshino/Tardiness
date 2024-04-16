14ec0f085d3a168924bc947c1c900bad3d109373
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2024-04-17 01:34:52 +0900

    Set platforms to latest ones since we're using Observation

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Tardiness",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v11),
-        .watchOS(.v6),
-        .tvOS(.v13),
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
