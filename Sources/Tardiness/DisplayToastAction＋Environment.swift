88ffc3ed09b45ffdf4d1fb50d90e824f608c1bad
Elvis Shi <3942121+el-hoshino@users.noreply.github.com>
2024-06-20 17:24:44 +0900

    Using callAsFunction instead of closure as DisplayToastAction

--- Sources/Tardiness/DisplayToastAction＋Environment.swift
+++ Sources/Tardiness/DisplayToastAction＋Environment.swift
@@ -6,8 +6,38 @@
 //
 
 import SwiftUI
+import OSLog
 
-public typealias DisplayToastAction = @MainActor (String) -> Void
+@MainActor
+public struct DisplayToastAction {
+    private let handler: ToastHandler?
+    private let logger = Logger()
+    init(handler: ToastHandler) {
+        self.handler = handler
+    }
+    private init() {
+        self.handler = nil
+    }
+    public func callAsFunction(_ toast: String) {
+        if let handler {
+            handler.queueMessage(toast)
+        } else {
+            let errorMessage = """
+                Calling DisplayToastAction with no ToastHandler. This means you forgot to set the environment.
+                Toast message: \(toast).
+                """
+            assertionFailure(errorMessage)
+            logger.warning("\(errorMessage)")
+        }
+    }
+}
+
+extension DisplayToastAction {
+    @MainActor
+    static func dummy() -> Self {
+        return .init()
+    }
+}
 
 public struct DisplayToastKey: EnvironmentKey {
     public static var defaultValue: DisplayToastAction? = nil
